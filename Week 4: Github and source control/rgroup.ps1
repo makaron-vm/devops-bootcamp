b4e916a91aed628cc8dc2282f1d8de53859d1d11
Adam Makarenko <makaron24@gmail.com>
2021-03-08 07:09:34 +0100

    delete md files from week 4

--- Week 4: Github and source control/rgroup.ps1
+++
@@ -1,6 +0,0 @@
-Get-AzIpGroup -Name test
-<<<<<<< HEAD
-Get-AzResource -ResourceType Microsoft.Resources/resourceGroups 
-=======
-get-azresourcegroup
->>>>>>> c71f0216aece0131d57bb384eee73a41fbb4a63a
